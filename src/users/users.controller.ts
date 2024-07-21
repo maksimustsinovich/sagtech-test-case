274dc2c92eb6da08460ed1dc356a91c956eea839
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-07-21 16:04:41 +0300

    feat: change users controller routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,43 +1,23 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { RegisterDto } from './dto/register.dto';
 import { UserDto } from './dto/user.dto';
-import { LoginDto } from './dto/login.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller()
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post('auth/register')
-  async register(@Body() registerDto: RegisterDto): Promise<UserDto> {
-    return await this.usersService.register(registerDto);
-  }
-
-  @Post('auth/login')
-  async login(@Body() loginDto: LoginDto): Promise<string> {
-    return await this.usersService.login(loginDto);
-  }
-
-  @Get('users')
+  @Get()
   async retrieveAllUsers(): Promise<UserDto[]> {
     return await this.usersService.getAllUsers();
   }
 
-  @Get('users/:id')
+  @Get(':id')
   async retrieveUserById(@Param('id') id: number): Promise<UserDto> {
     return await this.usersService.getUserById(id);
   }
 
-  @Put('users/:id')
+  @Put(':id')
   async updateUser(
     @Param('id') id: number,
     @Body() updateUserDto: UpdateUserDto,
@@ -45,7 +25,7 @@ export class UsersController {
     return await this.usersService.updateUser(id, updateUserDto);
   }
 
-  @Delete('users/:id')
+  @Delete(':id')
   async deleteUser(@Param('id') id: number): Promise<void> {
     return await this.usersService.deleteUser(id);
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,74 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { User } from './entity/user.entity';
 import { UserDto } from './dto/user.dto';
-import { RegisterDto } from './dto/register.dto';
-import { LoginDto } from './dto/login.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  private prisma: PrismaClient;
-
-  constructor() {
-    this.prisma = new PrismaClient();
-  }
-
-  async register(registerDto: RegisterDto): Promise<UserDto> {
-    const user = await this.prisma.user.create({ data: registerDto });
-    return {
-      id: user.id,
-      email: user.email,
-      role: user.role,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-    };
-  }
-
-  async login(loginDto: LoginDto): Promise<string> {
-    return 'placeholder';
-  }
+  constructor(private readonly prismaClient: PrismaClient) {}
 
   async getAllUsers(): Promise<UserDto[]> {
-    const users = await this.prisma.user.findMany();
-    return users.map((user: User) => ({
-      id: user.id,
-      email: user.email,
-      role: user.role,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-    }));
+    return await this.prismaClient.user.findMany();
   }
 
-  async getUserById(id: number): Promise<UserDto | null> {
-    const user = await this.prisma.user.findUnique({ where: { id } });
-    if (!user) {
-      return null;
-    }
-    return {
-      id: user.id,
-      email: user.email,
-      role: user.role,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-    };
+  async getUserById(id: number): Promise<UserDto> {
+    return await this.prismaClient.user.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
   async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<UserDto> {
-    const user = await this.prisma.user.update({
-      where: { id },
-      data: updateUserDto,
+    return await this.prismaClient.user.update({
+      where: {
+        id,
+      },
+      data: {
+        email: updateUserDto.email,
+        password: await bcrypt.hash(updateUserDto.password, 10),
+      },
     });
-    return {
-      id: user.id,
-      email: user.email,
-      role: user.role,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-    };
   }
 
   async deleteUser(id: number): Promise<void> {
-    await this.prisma.user.delete({ where: { id } });
+    await this.prismaClient.user.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
